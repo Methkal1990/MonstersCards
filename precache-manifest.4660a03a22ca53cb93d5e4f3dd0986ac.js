5a88de618eff2150701cc4ec1a32cde508a8ef55
methkal <methkal.khalawi.7e2@itb.cat>
2019-07-03 16:32:55 +0200

    Updates

---
+++ precache-manifest.4660a03a22ca53cb93d5e4f3dd0986ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa01cd04c992133349565bc54b91456",
+    "url": "/MonstersCards/index.html"
+  },
+  {
+    "revision": "e2dcc61ddbb37b628346",
+    "url": "/MonstersCards/static/css/main.1793c7d7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/MonstersCards/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e2dcc61ddbb37b628346",
+    "url": "/MonstersCards/static/js/main.7ceb78bb.chunk.js"
+  },
+  {
+    "revision": "e2e94e5944374bf100ba",
+    "url": "/MonstersCards/static/js/runtime~main.6a24bc33.js"
+  }
+]);
